6ca20c84742f3bd4b1247c9d5287f5f27528637f
Nehal Borole <nehalborole@Nehals-MacBook-Pro.local>
2020-04-28 09:14:08 +0530

    Removed unit tests

--- src/test/resources/FibWrongAnswer.java
+++
@@ -1,9 +0,0 @@
-public class Solution {
-    /**
-     * @param n id of fibonacci term to be returned
-     * @return  N'th term of Fibonacci sequence
-     */
-    public int fib(int n) {
-        return 0;
-    }
-}
--- src/test/resources/SumBinaryTree.java
+++
@@ -1,9 +0,0 @@
-import com.algocup.type.TreeNode;
-
-public class Solution {
-
-    public int sum(TreeNode root) {
-        if (root == null) return 0;
-        return root.data + sum(root.left) + sum(root.right);
-    }
-}
